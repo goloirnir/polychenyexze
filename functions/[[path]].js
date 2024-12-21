b377b0d64e8542f517560ca47ff633cb200fcf14
goloirnir <goloirnirs@gmail.com>
2024-12-21 15:55:26 +0100

    Update [[path]].js

--- functions/[[path]].js
+++ functions/[[path]].js
@@ -1,7 +1,7 @@
 export async function onRequest({ request }) {
 	const url = new URL(request.url)
 
-	const proxyUrl = 'https://diokiniebezaz.buzz'
+	const proxyUrl = 'https://ioeiurhneus.cyou'
 	const backendUrl = `${proxyUrl}${url.pathname}`
 
 	const response = await fetch(backendUrl, {
